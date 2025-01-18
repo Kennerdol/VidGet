0654e9d96dfe2c6ac6d4d2bbbc8d702c21e1f2fd
Kennerdol <kenny.t.mule.jnr@gmail.com>
2025-01-18 18:55:57 +0200

    Created class on main_panel.py and added an app title and tested it by running

--- main_panel.py
+++ main_panel.py
@@ -1,1 +1,15 @@
 #This is a main window for the application
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class MyApp(App):
+    def build(self):
+        self.icon = "assets/img/vg.png"  # Set the application icon (replace with your icon file path)
+        self.title = "Vidget"  # Set the application title
+        return Label(text="Youtube Downloader", font_size=72)
+
+
+if __name__ == "__main__":
+    MyApp().run()
